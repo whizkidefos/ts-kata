66b7c2a92a7f2cbdc12ee574b86c48e740915208
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2021-06-30 14:38:04 +0100

    wrote the failed function test

--- test/marsRover.test.ts
+++ test/marsRover.test.ts
@@ -8,4 +8,13 @@ describe("MarsRover", () => {
 
       expect(coordinates).toEqual("0:0:N");
    });
+
+   it("should return the coordinate 1:0:N if given M", () => {
+      const marsRover = new MarsRover();
+
+      const coordinates = marsRover.execute("M");
+
+      expect(coordinates).toEqual("1:0:N");
+   });
 });
+
