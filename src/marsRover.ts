dd6704e038172b0c7b8e86533ac18be1c186b2a9
Alasdair Smith <alasdairsmith0491@gmail.com>
2021-06-30 16:45:23 +0100

    rotate right one turn

--- src/marsRover.ts
+++ src/marsRover.ts
@@ -3,26 +3,11 @@ export default class MarsRover {
 
     execute(command: string) {
         let y = command.length;
-        // for(let i = 0; i < command.length; i++) {
-        //     y++;
-        // }
+
+        if(command === "R") {
+            return "0:0:E";
+        }
         return `0:${y}:N`;
-
-        // if(command === "M") {
-        //     return "0:1:N";
-        // }
-
-        // if(command === "MM") {
-        //     return "0:2:N";
-        // }
-
-        // if(command === "MMM") {
-        //     return "0:3:N";
-        // }
-
-        // if(command === "MMR") {
-        //     return "2:0:E";
-        // }
 
         return this.coordinates;
     }
