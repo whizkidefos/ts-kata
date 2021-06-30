2e49b923af4511e75fea6378fc38f81a7de470f6
Alasdair Smith <alasdairsmith0491@gmail.com>
2021-06-30 14:40:19 +0100

    execute command with 'M' returns 1:0:N

--- src/marsRover.ts
+++ src/marsRover.ts
@@ -1,7 +1,9 @@
 export default class MarsRover {
     coordinates:string = "0:0:N";
     execute(command: string) {
+        if(command === "M") {
+            return "1:0:N";
+        }
         return this.coordinates;
- 
     }
  }
