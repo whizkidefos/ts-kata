1d5b8e06569a9b1cb55bb2049494d62acb7741da
Alasdair Smith <alasdairsmith0491@gmail.com>
2021-07-01 10:31:47 +0100

    rotate left three times to face east

--- src/marsRover.ts
+++ src/marsRover.ts
@@ -37,6 +37,10 @@ export default class MarsRover {
             return this.x_and_y + SOUTH;
         }
 
+        if(command === ROTATE_LEFT + ROTATE_LEFT + ROTATE_LEFT) {
+            return this.x_and_y + EAST;
+        }
+
         return `0:${y}:N`;
 
         return this.coordinates;
